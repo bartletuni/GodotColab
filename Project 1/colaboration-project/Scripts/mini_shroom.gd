7400098004220bf4301abefc7d8bad57f1d0cfac
bartletuni <bartletuni@gmail.com>
2026-03-24 19:21:15 -0600

    Misc Changes (May be broken)
    Good Luck!

--- Project 1/colaboration-project/Scripts/mini_shroom.gd
+++ Project 1/colaboration-project/Scripts/mini_shroom.gd
@@ -1,12 +1,11 @@
 extends Area2D
-
-@onready var player: CharacterBody2D = $"../Player"
-@onready var health_bar: ProgressBar = $"../Player/HealthBar"
-@onready var additional_health: ProgressBar = $"../Player/AdditionalHealth"
+@onready var player: CharacterBody2D = $"../../Player"
+@onready var health_bar: ProgressBar = $"../../Player/HealthBar"
+@onready var additional_health: ProgressBar = $"../../Player/AdditionalHealth"
 
 func _on_body_entered(body: Node2D) -> void:
 	
-	if player.player_health == 5:
+	if player.player_health == 5 and player.player_shield < 5:
 		additional_health.value += 1
 		player.player_shield += 1
 		queue_free()
