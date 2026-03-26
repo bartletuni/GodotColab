4212527738c5a288821259ddd88c1b9b9925827d
AsianHalfSquat <63631967+AsianHalfSquat@users.noreply.github.com>
2026-03-26 04:06:04 -0600

    Goblin Wander scripts
    Tie Goblin now Idles and wanders randomly (within a certain radius). Pursuit will be added next.

--- Project 1/colaboration-project/Scripts/goblin_torch.gd
+++ Project 1/colaboration-project/Scripts/goblin_torch.gd
@@ -9,13 +9,38 @@ extends CharacterBody2D
 const GOBLIN_HEALTH = 3
 const GOBLIN_SPEED = 250
 
+enum EnemyState {IDLE, WANDER, FOLLOW}
+
 var enemy_health = GOBLIN_HEALTH
+var current_state = EnemyState.IDLE
+var home_position : Vector2
+var wander_target : Vector2
+
 
 func _ready() -> void:
-	pass
+	$IdleTimer.start()
+	home_position = global_position
 
 func _physics_process(delta: float) -> void:
-	pass
+	match current_state:
+		EnemyState.IDLE:
+			velocity = Vector2.ZERO
+			animated_sprite_2d.play("Idle")
+		EnemyState.WANDER:
+			var direction = (wander_target - global_position).normalized()
+			if direction.x < 0:
+				animated_sprite_2d.flip_h = true
+			elif direction.x > 0:
+				animated_sprite_2d.flip_h = false
+			velocity = direction * GOBLIN_SPEED/2
+			move_and_slide()
+			animated_sprite_2d.play("Walk")
+			if global_position.distance_to(wander_target) < 10:
+				current_state = EnemyState.IDLE
+				$IdleTimer.start()
+		EnemyState.FOLLOW:
+			pass
+
 
 
 func _on_detection_radius_area_entered(area: Area2D) -> void:
@@ -31,3 +56,11 @@ func _on_damage_box_area_entered(area: Area2D) -> void:
 		else:
 			enemy_health -= 1
 			health_bar.value = enemy_health
+
+
+func _on_idle_timer_timeout() -> void:
+	if current_state == EnemyState.IDLE:
+		var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+		var random_distance = randf_range(0, 400)
+		wander_target = home_position + (random_direction * random_distance)
+		current_state = EnemyState.WANDER
