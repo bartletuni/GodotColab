cc364daf7b7c11a67a12581b61db513f86c11562
bartletuni <bartletuni@gmail.com>
2026-03-29 13:51:16 -0600

    Added Health to Spawner

--- Project 1/colaboration-project/Scripts/mob_spawner.gd
+++ Project 1/colaboration-project/Scripts/mob_spawner.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
 @onready var timer: Timer = $Timer
+@onready var hitbox: Area2D = $hitbox
+@onready var hitbox_shape: CollisionShape2D = $hitbox/hitbox_shape
+@onready var health_bar: ProgressBar = $HealthBar
+
+var spawner_health = 10
 
 func enemy_spawning():
 	
@@ -10,7 +15,6 @@ func enemy_spawning():
 		path_follow_2d.progress_ratio = randf()
 		goblin_red.global_position = path_follow_2d.global_position
 		add_sibling(goblin_red)
-		goblin_red.EnemyState.FOLLOW
 		await get_tree().create_timer(3).timeout
 
 func _on_area_entered(area: Area2D) -> void:
@@ -30,3 +34,14 @@ func _on_area_exited(area: Area2D) -> void:
 
 func _on_timer_timeout() -> void:
 	enemy_spawning()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player Objects"):
+		if spawner_health == 1:
+			health_bar.value = spawner_health - 1
+			queue_free()
+		else:
+			spawner_health -= 1
+			health_bar.value = spawner_health
+			
--- Project 1/colaboration-project/Scripts/player.gd
+++ Project 1/colaboration-project/Scripts/player.gd
@@ -104,7 +104,6 @@ func _physics_process(_delta: float) -> void:
 	
 	move_and_slide()
 	
-#
 #func _on_timer_timeout() -> void:
 	#animated_sprite_2d.play("death")
 
